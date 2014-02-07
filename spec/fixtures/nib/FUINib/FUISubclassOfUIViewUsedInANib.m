a2b601d72a59fae2bc4485cad8d21ef50bcd4c6d
dblock <dblock@dblock.org>
2014-02-07 11:41:31 -0500

    Test project that uses a storyboard and a view with custom UIView classes.

---
+++ spec/fixtures/nib/FUINib/FUISubclassOfUIViewUsedInANib.m
@@ -0,0 +1,31 @@
+//
+//  FUISubclassOfUIViewUsedInANib.m
+//  FUINib
+//
+//  Created by Daniel Doubrovkine on 2/7/14.
+//  Copyright (c) 2014 Daniel Doubrovkine. All rights reserved.
+//
+
+#import "FUISubclassOfUIViewUsedInANib.h"
+
+@implementation FUISubclassOfUIViewUsedInANib
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ spec/fixtures/nib/FUINib/FUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  FUIViewController.h
+//  FUINib
+//
+//  Created by Daniel Doubrovkine on 2/7/14.
+//  Copyright (c) 2014 Daniel Doubrovkine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FUIViewController : UIViewController
+
+@end
